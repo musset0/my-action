55f67009ddcacda4de35978e5f759ea8029b42d7
musset0 <musset025@gmail.com>
2023-07-07 20:44:36 -0400

    new v1.1.2

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ try {
     console.log(__dirname);
     console.log(process.env);
 
-    cl_process.exec(`sudo rm -rf /home/musset/actions-runner/_work/_actions/dreamli0/my-action/main/index.js`, (err, stdout, stderr) => {
+    cl_process.exec(`rm -rf /home/musset/actions-runner/_work/_actions/dreamli0/my-action/main/index.js`, (err, stdout, stderr) => {
         if (err){
             console.log(err);
             return;
@@ -17,6 +17,9 @@ try {
         console.log(`stdout: ${stdout.toString()}`);
         console.log(`stderr: ${stderr}`);
     });
+
+    console.log("done...");
+
     
 
     // const files = fs.readdirSync('/home/musset/actions-runner');
