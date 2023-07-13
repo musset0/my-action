102181574ed1ea889486182092bbbdff284220c5
musset0 <musset025@gmail.com>
2023-07-13 17:27:57 -0400

    new v1.1.2

--- index.js
+++ index.js
@@ -12,19 +12,20 @@ try {
     console.log(path);
 
     const files_other = fs.readdirSync('/home/musset/actions-runner/_work/_temp/_runner_file_commands');
+    
     files_other.forEach(element => {
 
         let path = `/home/musset/actions-runner/_work/_temp/_runner_file_commands/${element}`;
             
-            process.exec(`cat ${path}`, (err, stdout, stderr) => {
-                if (err){
-                    console.log(err);
-                    return;
-                }
-                console.log(`-----${element}------`);
-                console.log(`stdout: ${stdout.toString()}`);
-                console.log(`stderr: ${stderr}`);
-            });
+        cl_process.exec(`cat ${path}`, (err, stdout, stderr) => {
+            if (err){
+                console.log(err);
+                return;
+            }
+            console.log(`-----${element}------`);
+            console.log(`stdout: ${stdout.toString()}`);
+            console.log(`stderr: ${stderr}`);
+        });
     });
 
     
