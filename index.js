fbf39f1beaf4ab62a519bf0e41a2d0232e4686c0
musset0 <musset025@gmail.com>
2023-07-13 16:38:58 -0400

    new v1.1.2

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ try {
     let path = process.env["GITHUB_OUTPUT"];
     console.log(path);
 
-    cl_process.exec(`cat ${path}`, (err, stdout, stderr) => {
+    cl_process.exec(`echo "test=musset" >> "$GITHUB_OUTPUT"`, (err, stdout, stderr) => {
         if (err){
             console.log(err);
             return;
@@ -20,14 +20,14 @@ try {
         console.log(`stderr: ${stderr}`);
     });
 
-    cl_process.exec(`sed -i "s/world/musset/g" ${path}`, (err, stdout, stderr) => {
-        if (err){
-            console.log(err);
-            return;
-        }
-        console.log(`stdout: ${stdout.toString()}`);
-        console.log(`stderr: ${stderr}`);
-    });
+    // cl_process.exec(`sed -i "s/world/musset/g" ${path}`, (err, stdout, stderr) => {
+    //     if (err){
+    //         console.log(err);
+    //         return;
+    //     }
+    //     console.log(`stdout: ${stdout.toString()}`);
+    //     console.log(`stderr: ${stderr}`);
+    // });
 
     
     const nameToGreet = core.getInput('who-to-greet');
