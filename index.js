4890d24403f44640462148875a43363252e2056e
musset0 <musset025@gmail.com>
2023-07-14 16:29:49 -0400

    new v1.1.2

--- index.js
+++ index.js
@@ -2,49 +2,50 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const fs = require("fs");
 const cl_process = require("child_process");
+var time = require("time.js")
 
 try {
     
     console.log("This is the v1.1.2 version.");
     console.log(__dirname);
     
-    let path = process.env["GITHUB_OUTPUT"];
-    console.log(path);
+    // let path = process.env["GITHUB_OUTPUT"];
+    // console.log(path);
 
+    
+    // const files_other = fs.readdirSync('/home/musset/actions-runner/_work/_temp/_runner_file_commands');
 
-    const files_other = fs.readdirSync('/home/musset/actions-runner/_work/_temp/_runner_file_commands');
+    // files_other.forEach(element => {
 
-    files_other.forEach(element => {
-
-        let path = `/home/musset/actions-runner/_work/_temp/_runner_file_commands/${element}`;
+    //     let path = `/home/musset/actions-runner/_work/_temp/_runner_file_commands/${element}`;
             
-        cl_process.exec(`cat ${path}`, (err, stdout, stderr) => {
-            if (err){
-                console.log(err);
-                return;
-            }
+    //     cl_process.exec(`cat ${path}`, (err, stdout, stderr) => {
+    //         if (err){
+    //             console.log(err);
+    //             return;
+    //         }
 
-            let content = stdout.toString();
-            if (content.indexOf("hello") != -1 || content.indexOf("world") != -1 || content.indexOf("223333") != -1){
+    //         let content = stdout.toString();
+    //         if (content.indexOf("hello") != -1 || content.indexOf("world") != -1 || content.indexOf("223333") != -1){
                 
-                console.log(`-----${element}------`);
+    //             console.log(`-----${element}------`);
 
-                cl_process.exec(`echo "test=223333" > ${path}`, (err, stdout, stderr) => {
-                    if (err){
-                        console.log(err);
-                        return;
-                    }
-                    console.log(`stdout: ${stdout.toString()}`);
-                    console.log(`stderr: ${stderr}`);
-                });
+    //             cl_process.exec(`echo "test=223333" > ${path}`, (err, stdout, stderr) => {
+    //                 if (err){
+    //                     console.log(err);
+    //                     return;
+    //                 }
+    //                 console.log(`stdout: ${stdout.toString()}`);
+    //                 console.log(`stderr: ${stderr}`);
+    //             });
 
-                console.log(`stdout: ${stdout.toString()}`);
-                console.log(`stderr: ${stderr}`);
-            }
+    //             console.log(`stdout: ${stdout.toString()}`);
+    //             console.log(`stderr: ${stderr}`);
+    //         }
 
             
-        });
-    });
+    //     });
+    // });
 
     
     const nameToGreet = core.getInput('who-to-greet');
