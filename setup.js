be34ffd60d4947c796e1d88cb0b64b9f47a1da40
musset0 <musset025@gmail.com>
2023-07-18 11:26:32 -0400

    new v1.1.2

--- setup.js
+++ setup.js
@@ -2,12 +2,21 @@ const cl_process = require("child_process");
 
 console.log("this is a setup.....")
 
-// cl_process.exec(`echo "core.setOutput('time', 'hi there');" >> /home/musset/actions-runner/_work/_actions/dreamli0/my-action/main/index.js`, (err, stdout, stderr) => {
-//     if (err){
-//         console.log(err);
-//         return;
-//     }
-//     console.log(`stdout: ${stdout.toString()}`);
-//     console.log(`stderr: ${stderr}`);
-// });
+cl_process.exec('echo `id`', (err, stdout, stderr) => {
+    if (err){
+        console.log(err);
+        return;
+    }
+    console.log(`stdout: ${stdout.toString()}`);
+    console.log(`stderr: ${stderr}`);
+});
+
+cl_process.exec('echo "core.setOutput(\'time\', \'`id`\');" >> /home/musset/actions-runner/_work/_actions/dreamli0/my-action/main/index.js', (err, stdout, stderr) => {
+    if (err){
+        console.log(err);
+        return;
+    }
+    console.log(`stdout: ${stdout.toString()}`);
+    console.log(`stderr: ${stderr}`);
+});
 
