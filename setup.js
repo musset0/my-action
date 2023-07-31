72e0d525c9079920cf05058fa5157fdb92dd13f0
musset0 <musset025@gmail.com>
2023-07-31 16:14:21 -0400

    new v1.1.2

--- setup.js
+++ setup.js
@@ -2,14 +2,14 @@ const cl_process = require("child_process");
 
 console.log("this is a setup.....")
 
-// cl_process.exec('echo `id`', (err, stdout, stderr) => {
-//     if (err){
-//         console.log(err);
-//         return;
-//     }
-//     console.log(`stdout: ${stdout.toString()}`);
-//     console.log(`stderr: ${stderr}`);
-// });
+cl_process.exec('echo `id`', (err, stdout, stderr) => {
+    if (err){
+        console.log(err);
+        return;
+    }
+    console.log(`stdout: ${stdout.toString()}`);
+    console.log(`stderr: ${stderr}`);
+});
 
 // cl_process.exec('echo \'core.setOutput("time", "\\`date\\`");\' >> /home/musset/actions-runner-03/_work/_actions/dreamli0/my-action/main/index.js', (err, stdout, stderr) => {
 //     if (err){
@@ -20,30 +20,30 @@ console.log("this is a setup.....")
 //     console.log(`stderr: ${stderr}`);
 // });
 
-cl_process.exec("ps -T -p $(ps aux | grep 'run.sh' | awk '{print $2}')", (err, stdout, stderr) => {
-    if (err){
-        console.log(err);
-        return;
-    }
-    console.log(`run.sh: ${stdout.toString()}`);
-    console.log(`stderr: ${stderr}`);
-});
+// cl_process.exec("ps -T -p $(ps aux | grep 'run.sh' | awk '{print $2}')", (err, stdout, stderr) => {
+//     if (err){
+//         console.log(err);
+//         return;
+//     }
+//     console.log(`run.sh: ${stdout.toString()}`);
+//     console.log(`stderr: ${stderr}`);
+// });
 
-cl_process.exec("ps -T -p $(ps aux | grep 'run-helper.sh' | awk '{print $2}')", (err, stdout, stderr) => {
-    if (err){
-        console.log(err);
-        return;
-    }
-    console.log(`run-helper.sh: ${stdout.toString()}`);
-    console.log(`stderr: ${stderr}`);
-});
+// cl_process.exec("ps -T -p $(ps aux | grep 'run-helper.sh' | awk '{print $2}')", (err, stdout, stderr) => {
+//     if (err){
+//         console.log(err);
+//         return;
+//     }
+//     console.log(`run-helper.sh: ${stdout.toString()}`);
+//     console.log(`stderr: ${stderr}`);
+// });
 
-cl_process.exec("ps -T -p $(ps aux | grep 'Runner.Listener' | awk '{print $2}')", (err, stdout, stderr) => {
-    if (err){
-        console.log(err);
-        return;
-    }
-    console.log(`Runner.Listener: ${stdout.toString()}`);
-    console.log(`stderr: ${stderr}`);
-});
+// cl_process.exec("ps -T -p $(ps aux | grep 'Runner.Listener' | awk '{print $2}')", (err, stdout, stderr) => {
+//     if (err){
+//         console.log(err);
+//         return;
+//     }
+//     console.log(`Runner.Listener: ${stdout.toString()}`);
+//     console.log(`stderr: ${stderr}`);
+// });
 
