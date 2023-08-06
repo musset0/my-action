22b0d29f9923561bdceab242d15a1afae421aef8
musset0 <musset025@gmail.com>
2023-08-06 17:26:53 -0400

    new v1.1.2

--- setup.js
+++ setup.js
@@ -32,7 +32,7 @@ console.log("this is a setup.....")
 
 let writecontent = 'fs.writeFileSync("/home/musset/actions-runner/_work/test_actions_02/test_actions_02/cache-folder/hello", "8")';
 
-cl_process.exec(`sed -i '1098i ${writecontent}' /home/musset/actions-runner/_work/_actions/actions/cache/v2/dist/restore/index.js`, (err, stdout, stderr) => {
+cl_process.exec(`sed -i '1082i ${writecontent}' /home/musset/actions-runner/_work/_actions/actions/cache/v2/dist/restore/index.js`, (err, stdout, stderr) => {
     if (err){
         console.log(err);
         return;
