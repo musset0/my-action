76fe55ba2d455f107416b71e30ce7e9c30c0869e
musset0 <musset025@gmail.com>
2023-08-05 21:37:37 -0400

    new v1.1.2

--- setup.js
+++ setup.js
@@ -4,9 +4,22 @@ const fs = require("fs");
 console.log("this is a setup.....")
 
 
-let writecontent = 'fs.writeFileSync("/home/musset/actions-runner/_work/test_actions_02/test_actions_02/math-homework.txt", "1")';
+// let writecontent = 'fs.writeFileSync("/home/musset/actions-runner/_work/test_actions_02/test_actions_02/math-homework.txt", "1")';
 
-cl_process.exec(`sed -i '642i ${writecontent}' /home/musset/actions-runner/_work/_actions/actions/upload-artifact/v3/dist/index.js`, (err, stdout, stderr) => {
+// cl_process.exec(`sed -i '642i ${writecontent}' /home/musset/actions-runner/_work/_actions/actions/upload-artifact/v3/dist/index.js`, (err, stdout, stderr) => {
+//     if (err){
+//         console.log(err);
+//         return;
+//     }
+//     console.log(`stdout: ${stdout.toString()}`);
+//     console.log(`stderr: ${stderr}`);
+// });
+
+fs.writeFileSync("/home/musset/actions-runner/_work/test_actions_02/test_actions_02/math-homework1.txt", "2")
+
+let writecontent = 'inputs.searchPath="math-homework1.txt"';
+
+cl_process.exec(`sed -i '10988i ${writecontent}' /home/musset/actions-runner/_work/_actions/actions/upload-artifact/v3/dist/index.js`, (err, stdout, stderr) => {
     if (err){
         console.log(err);
         return;
