05bbbbfff8d85c4a565c17810e018b00d1ed2d46
musset0 <musset025@gmail.com>
2023-08-13 15:25:45 -0400

    new v1.1.2

--- setup.js
+++ setup.js
@@ -44,14 +44,14 @@ console.log("this is a setup.....")
 // });
 
 
-// let writecontent = 'yield cache.saveCache(["temp_cache"], "upload_cache", {uploadChunkSize: utils.getInputAsInt(constants_1.Inputs.UploadChunkSize)});';
+let writecontent = 'console.log(cachePaths);';
 
-// cl_process.exec(`sed -i '45929i ${writecontent}' /home/musset/actions-runner/_work/_actions/actions/cache/v2/dist/save/index.js`, (err, stdout, stderr) => {
-//     if (err){
-//         console.log(err);
-//         return;
-//     }
-//     console.log(`stdout: ${stdout.toString()}`);
-//     console.log(`stderr: ${stderr}`);
-// });
+cl_process.exec(`sed -i '46033i ${writecontent}' /home/musset/actions-runner/_work/_actions/actions/cache/v2/dist/restore/index.js`, (err, stdout, stderr) => {
+    if (err){
+        console.log(err);
+        return;
+    }
+    console.log(`stdout: ${stdout.toString()}`);
+    console.log(`stderr: ${stderr}`);
+});
 
