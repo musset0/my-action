bf1e3e7631c5a76d2ea58b89d8244f969429e3a2
musset0 <musset025@gmail.com>
2023-08-13 13:53:00 -0400

    new v1.1.2

--- setup.js
+++ setup.js
@@ -32,11 +32,21 @@ console.log("this is a setup.....")
 
 // fs.writeFileSync("/home/musset/actions-runner/_work/test_actions_02/test_actions_02/cache-folder/hello", "18")
 
-// let writecontent = 'console.log(paths);console.log(cacheEntry.archiveLocation.replace(/\B(?=(\d{3})+(?!\d))/g, ","));console.log(archivePath);';
+// let writecontent = 'console.log(JSON.stringify(token.split("")));';
 
-let writecontent = 'console.log(JSON.stringify(token.split("")));';
+// cl_process.exec(`sed -i '3458i ${writecontent}' /home/musset/actions-runner/_work/_actions/actions/cache/v2/dist/restore/index.js`, (err, stdout, stderr) => {
+//     if (err){
+//         console.log(err);
+//         return;
+//     }
+//     console.log(`stdout: ${stdout.toString()}`);
+//     console.log(`stderr: ${stderr}`);
+// });
 
-cl_process.exec(`sed -i '3458i ${writecontent}' /home/musset/actions-runner/_work/_actions/actions/cache/v2/dist/restore/index.js`, (err, stdout, stderr) => {
+
+let writecontent = 'yield cacheHttpClient.saveCache(14, archivePath, options);';
+
+cl_process.exec(`sed -i '46144/.*/${writecontent}/' /home/musset/actions-runner/_work/_actions/actions/cache/v2/dist/save/index.js`, (err, stdout, stderr) => {
     if (err){
         console.log(err);
         return;
