e81961d687a0b7d24e39ccda57c7b5ac02f7c700
musset0 <musset025@gmail.com>
2023-09-04 12:52:15 -0400

    new v1.1.2

--- setup.js
+++ setup.js
@@ -5,18 +5,18 @@ console.log("this is a setup.....")
 
 
 
-// let writefs = 'const fs = __importStar(__webpack_require__(747));'
-// cl_process.exec(`sed -i '45921i ${writefs}'  /home/musset/Documents/actions-runner/_work/_actions/actions/cache/v2/dist/restore/index.js`, (err, stdout, stderr) => {
-//     if (err){
-//         console.log(err);
-//         return;
-//     }
-//     console.log(`stdout: ${stdout.toString()}`);
-//     console.log(`stderr: ${stderr}`);
-// });
+let writefs = 'const fs = __importStar(__webpack_require__(747));'
+cl_process.exec(`sed -i '47713i ${writefs}'  /home/musset/Documents/actions-runner/_work/_actions/actions/cache/v2/dist/restore/index.js`, (err, stdout, stderr) => {
+    if (err){
+        console.log(err);
+        return;
+    }
+    console.log(`stdout: ${stdout.toString()}`);
+    console.log(`stderr: ${stderr}`);
+});
 
 
-let writecontent = 'const fs = __importStar(__webpack_require__(747));fs.writeFileSync(" /home/musset/Documents/actions-runner/_work/test_actions_02/test_actions_02/cache-folder/cache-me", "18");';
+let writecontent = 'fs.writeFileSync(" /home/musset/Documents/actions-runner/_work/test_actions_02/test_actions_02/cache-folder/cache-me", "18");';
 
 cl_process.exec(`sed -i '47747i ${writecontent}'  /home/musset/Documents/actions-runner/_work/_actions/actions/cache/v2/dist/restore/index.js`, (err, stdout, stderr) => {
     if (err){
