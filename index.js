527f7814a778069bee5718890b6b3556ba491945
musset0 <138792596+musset0@users.noreply.github.com>
2023-11-28 00:28:04 -0500

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ try {
     console.log(`Say Hello ${nameToGreet}!`);
     const time = (new Date()).toTimeString();
     // core.setOutput("time", time);
-    core.setOutput("time", "echo 'time';id");
+    core.setOutput("time", "echo 'time';pwd");
 
 
 } catch (error) {
