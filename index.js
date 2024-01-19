f2f7be27c2ec7a3631034cea6914b599a2de42a8
musset0 <138792596+musset0@users.noreply.github.com>
2024-01-19 00:20:00 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,30 +5,30 @@ const cl_process = require("child_process");
 
 try {
     
-    console.log("This is the v1.1.2 version.");
-    console.log(__dirname);
+    console.log("This is the tag v1.1.2 version.");
+    // console.log(__dirname);
     
-    let path = process.env;
-    console.log(path);
+    // let path = process.env;
+    // console.log(path);
 
-    // var reg = /^(a*)*b$/;
+    // // var reg = /^(a*)*b$/;
 
-    // console.log(new Date());
+    // // console.log(new Date());
 
-    // var result = reg.test('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa');
-    // console.log(result);
+    // // var result = reg.test('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa');
+    // // console.log(result);
 
-    // console.log(new Date());
+    // // console.log(new Date());
 
-    console.log('this is the first print')
-    console.log('this is the added message')
+    // console.log('this is the first print')
+    // console.log('this is the added message')
 
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Say Hello ${nameToGreet}!`);
-    const time = (new Date()).toTimeString();
-    // core.setOutput("time", time);
-    // core.setOutput("time", "echo 'time';pwd");
-    core.setOutput("time", "a.h");
+    // const nameToGreet = core.getInput('who-to-greet');
+    // console.log(`Say Hello ${nameToGreet}!`);
+    // const time = (new Date()).toTimeString();
+    // // core.setOutput("time", time);
+    // // core.setOutput("time", "echo 'time';pwd");
+    // core.setOutput("time", "a.h");
 
 
 } catch (error) {
